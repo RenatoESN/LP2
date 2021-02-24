ded19b118019ffac2b1260642f1dd0a0d08d2557
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-02-24 10:22:09 -0300

    Create Hello2DApp.java

---
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hello2DApp {
+    public static void main (String[] args) {
+        Hello2DFrame frame = new Hello2DFrame();
+        frame.setVisible(true);
+    }
+}
+
+class Hello2DFrame extends JFrame {
+    public Hello2DFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java2D - Hello World!");
+        this.setSize(350, 350);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        int w = getWidth();
+        int h = getHeight();
+        g.setColor(Color.blue);
+        g.fillRect(0, 0, w, h);
+        g2d.setPaint(Color.red);
+        g2d.drawLine(87,0, 87,h);
+        g2d.drawLine(0,87, w,87);
+        g2d.drawLine(262,0, 262,h);
+        g2d.drawLine(0,262, w,262);
+    }
+}
