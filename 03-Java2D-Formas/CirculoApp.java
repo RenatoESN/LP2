dc8d9c3d787bb14bc15fe69fa3d64b0fb875eb45
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-03-10 09:45:42 -0300

    Update CirculoApp.java

--- 03-Java2D-Formas/CirculoApp.java
+++ 03-Java2D-Formas/CirculoApp.java
@@ -1,10 +1,12 @@
 class Circulo{
     int x, y;
-    int raio
+    int raio;
     Circulo (int posx, int posy, int raio){
         this.x = posx;
         this.y = posy;
         this.raio = raio;
     }
-    
+    void print(){
+        System.out.println("Circulo na posicao %d e %d e de raio %d", this.x, this.y, this.raio);
+    }
 }
