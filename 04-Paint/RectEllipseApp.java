894efe663afb1d03bf064ff11e2f95e131cde163
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-03-20 13:50:55 -0300

    Update RectEllipseApp.java

--- 04-Paint/RectEllipseApp.java
+++ 04-Paint/RectEllipseApp.java
@@ -26,9 +26,9 @@ class RectEllipseFrame extends JFrame {
         );
         this.setTitle("Rect + Ellipse");
         this.setSize(500, 500);
-        this.e1 = new Ellipse(50,100, 100,30);
-        this.e2 = new Ellipse(250,300, 90,80);
-        this.e3 = new Ellipse(200,100, 150,130);
+        this.e1 = new Ellipse(50,100, 100,30, new Color(25, 214, 232), new Color(232, 25, 25));
+        this.e2 = new Ellipse(250,300, 90,80, new Color(232, 25, 180), new Color(25, 235, 32));
+        this.e3 = new Ellipse(200,100, 150,130, new Color(25, 232, 218), new Color(25, 232, 46));
     }
 
     public void paint (Graphics g) {
@@ -42,21 +42,27 @@ class RectEllipseFrame extends JFrame {
 class Ellipse {
     int x, y;
     int w, h;
+    Color background;
+    Color line;
 
-    Ellipse (int x, int y, int w, int h) {
+    Ellipse (int x, int y, int w, int h, Color background, Color line) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.background = background;
+        this.line = line;       
     }
 
     void print () {
-        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",this.w, this.h, this.x, this.y);
     }
 
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(line);
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setColor(background);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
     }
 }
