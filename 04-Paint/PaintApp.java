df40e08a1f51c678127e342215adf868f463247a
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-03-20 12:44:00 -0300

    Update PaintApp.java

--- 04-Paint/PaintApp.java
+++ 04-Paint/PaintApp.java
@@ -11,6 +11,8 @@ class PaintApp {
 
 class PaintFrame extends JFrame {
     Rect r1;
+    Rect r2;
+    Rect r3;
 
     PaintFrame () {
         this.addWindowListener (
@@ -21,25 +23,33 @@ class PaintFrame extends JFrame {
             }
         );
         this.setTitle("Painting Figures");
-        this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30);
+        this.setSize(500, 500);
+        this.r1 = new Rect(50,200, 100,30);
+        this.r2 = new Rect(200,200, 100,30);
+        this.r3 = new Rect(350,200, 100,30);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         this.r1.paint(g);
+        this.r2.paint(g);
+        this.r3.paint(g);
     }
 }
 
 class Rect {
     int x, y;
     int w, h;
+   Color background;
+   Color line;
     
-    Rect (int x, int y, int w, int h) {
+    Rect (int x, int y, int w, int h, Color background, Color line) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.background = background;
+        this.line = line;
     }
 
     void print () {
