f76f48ae2fce45e25bf9da65255b2cae170fe841
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-04-05 09:59:43 -0300

    Update Figure.java

--- 07-Figure/figures/Figure.java
+++ 07-Figure/figures/Figure.java
@@ -1,7 +1,11 @@
 package figures;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class Figure {
+	int x, y, w, h;
+	Color line, background;
+	public abstract void print ();
     public abstract void paint (Graphics g);
 }
