bc4971df720e5d6bea4fbaac9ff82c2b1a39aaee
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-04-10 12:14:48 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 
 public abstract class Figure {
 	int x, y, w, h;
-	Color line, background;
+	//Color line, background;
 
     public Figure (int x, int y, int w, int h){
         this.x = x;
