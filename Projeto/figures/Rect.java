a6c890536cc82c40fba6e51135c7f8d17a7d99b5
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-04-12 11:20:24 -0300

    Inicio Projeto

---
+++ Projeto/figures/Rect.java
@@ -0,0 +1,21 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure{
+    int x, y;
+    int w, h;
+
+    public Rect (int x, int y, int w, int h){
+        super(x,y,w,h);
+    }
+
+    public void print(){
+        System.out.format("Rentangulo de tamanho (%d, %d) e na posicao (%d, %d).\n", this.w, this.h, this.x, this.y);
+    }
+    
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
