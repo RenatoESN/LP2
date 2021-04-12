227fdd8eab6520fcbafd37b7fb3d778cbb514479
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-04-12 08:58:09 -0300

    Update

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -12,6 +12,11 @@ public class Ellipse extends Figure {
         super(x, y, w, h);
     }
 
+    public void print () {
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
