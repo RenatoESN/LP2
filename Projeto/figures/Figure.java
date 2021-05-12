c457b0591a22e549e75f85a8e5707d46cf5f3415
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2021-05-12 09:46:56 -0300

    IVisible creation

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -1,8 +1,9 @@
 package figures;
 
+import ivisible.IVisible;
 import java.awt.*;
 
-public abstract class Figure {
+public abstract class Figure implements IVisible{
     int x, y;
     int w, h;
     Color line, background;
@@ -39,6 +40,9 @@ public abstract class Figure {
     public void drag(int novax, int novay){
         this.x = novax;
         this.y = novay;
+    }
+    public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
 
 }
